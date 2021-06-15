54666fa2637670b105f5e41fd70cd5c342d1a17b
root <root@srvcent03.knowhere>
2021-06-15 16:02:16 +0530

    changed to feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <p>Burn in the fires of HELL!!!!</p>
+          <p>You are now in feature branch and you will still Burn in the fires of HELL!!!!</p>
         </p>
         <a
           className="App-link"
